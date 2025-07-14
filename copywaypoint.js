353bda6ba2aa352fe57ce95a86ac3cb500a683b7
Gareth Hughes <g_github@flamescape.com>
2025-07-14 14:57:07 +0100

    invert second coord on copy

--- copywaypoint.js
+++ copywaypoint.js
@@ -1,5 +1,5 @@
 
 async function copyWaypoint(x, y, idx, total) {
-    const cmd = `/waypoint addati x ${x} 0 ${y} true lime "TL Waypoint ${idx}/${total}"`;
+    const cmd = `/waypoint addati x ${x} 0 ${-y} true lime "TL Waypoint ${idx}/${total}"`;
     await window.navigator.clipboard.writeText(cmd);
 }
